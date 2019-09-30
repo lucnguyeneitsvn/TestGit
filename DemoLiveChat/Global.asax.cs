5c7cb59a104c56c46ccf4fb4a5c5fd17cf7720e8
Luc Nguyen Tan <tanluc.nguyen@EITSVN.COM>
2019-09-30 13:16:49 +0700

    Change global.asx

--- DemoLiveChat/Global.asax.cs
+++ DemoLiveChat/Global.asax.cs
@@ -16,7 +16,6 @@ namespace DemoLiveChat
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            var npk = 1;
         }
     }
 }
