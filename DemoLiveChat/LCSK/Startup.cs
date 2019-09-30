a6b85fbdc72942116f4a01bf9c581c994a1e3b7c
Luc Nguyen Tan <tanluc.nguyen@EITSVN.COM>
2019-09-30 12:03:14 +0700

    Init

---
+++ DemoLiveChat/LCSK/Startup.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DemoLiveChat.LCSK.Startup))]
+
+namespace DemoLiveChat.LCSK
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
